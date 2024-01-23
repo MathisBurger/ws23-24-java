05097b419c3729bb152687fda313875a070aa674
Mathis Burger <handymathis@gmail.com>
2024-01-23 16:42:09 +0100

    [TASK] Added simple test class for testing some things

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String... args) {
+        System.out.println((""+'c').repeat(5));
+        System.out.println(-1 >>> 1);
+        System.out.println(-1 >> 1);
+    }
+}
