bae5c74d0cb6c9d2c1093b37d76b4fe1dfe690d7
Mathis Burger <handymathis@gmail.com>
2024-01-23 16:43:05 +0100

    [TASK] More test cases

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println((""+'c').repeat(5));
         System.out.println(-1 >>> 1);
         System.out.println(-1 >> 1);
+        double x = 3.0 + 'c';
     }
 }
